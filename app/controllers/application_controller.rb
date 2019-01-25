dffd84328c2387fb191dafac273a6fbba6bc1e57
Andrew Johnson <ajohnson.uva@gmail.com>
2019-01-25 10:21:45 -0500

    added users and events folders to views file tree

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,26 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :sessions, true
+    set :session_secret, ENV.fetch("SESSION_SECRET")
   end
 
   get "/" do
-    erb :welcome
+    if logged_in?
+      redirect :'/events'
+    else
+      erb :welcome
+    end
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
   end
 
 end
