9f2dbeb977cbebd81478cc224867d1951174bafb
Andrew Johnson <ajohnson.uva@gmail.com>
2019-01-25 10:37:32 -0500

    stubbed login and signup erb files and controller routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,19 @@
 class SessionsController < ApplicationController
 
   get '/login' do
-
+    if logged_in?
+      redirect :"/events"
+    else
+      erb :login
+    end
   end
 
   get '/signup' do
-
+    if logged_in?
+      redirect :'/events'
+    else
+      erb :signup
+    end
   end
-  
+
 end
