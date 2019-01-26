e3a397e7c9590b327f507f50bd5ae2ff1b578ca6
Andrew Johnson <ajohnson.uva@gmail.com>
2019-01-26 11:43:00 -0500

    edited login validation

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,18 @@ class SessionsController < ApplicationController
     end
   end
 
+  post '/login' do
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect :'/events'
+    else
+      binding.pry
+      @no_user = true unless @user
+      erb :login
+    end
+  end
+
   get '/signup' do
     if logged_in?
       redirect :'/events'
