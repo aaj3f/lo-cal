06fabab426ce0117ef62589da49152a03a1edeb0
Andrew Johnson <ajohnson.uva@gmail.com>
2019-01-29 11:54:07 -0500

    added post and patch routes to Rsvps_Controller

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,4 +1,31 @@
 class RsvpsController < ApplicationController
 
+  post '/rsvps' do
+    binding.pry
+    event = Event.find_by_id(params[:event])
+    response = params[:rsvp]
+    user = User.find_by_id(session[:user_id])
+    if event && user && response && !(user.events.include?(event))
+      rsvp = Rsvp.create(user_id: user.id, event_id: event.id, status: response)
+      redirect :"/events/#{event.id}"
+    else
+      redirect :"/events"
+    end
+  end
+
+  patch '/rsvps/:id' do
+    event = Event.find_by_id(params[:id])
+    new_response = params[:rsvp]
+    user = User.find_by_id(session[:user_id])
+    if event && user && new_response && user.events.include?(event)
+      Rsvp.find_by(user_id: user.id, event_id: event.id).update(status: new_response)
+      redirect :"/events/#{event.id}"
+    else
+      redirect :"/events"
+    end
+  end
 
 end
+
+
+##Can be refactored so that we don't have to initialize `user` but just use current_user & logged_in? helpers
