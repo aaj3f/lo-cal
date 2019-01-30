1f028c1ca11ec3ba66cd8fd40b246d0d60dc1bab
Andrew Johnson <ajohnson.uva@gmail.com>
2019-01-30 13:08:19 -0500

    added flash error messages to signup route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,9 +15,11 @@ class SessionsController < ApplicationController
       redirect :"/users/#{@user.id}"
     elsif @user && !(@user.authenticate(params[:user][:password]))
       @user.errors.add(:password, "does not match our records.")
+      flash[:error] = "That password does not match our records."
       erb :login
     else
       @no_user = true unless @user
+      flash[:error] = "That email address is not registered to a current user."
       erb :login
     end
   end
@@ -31,9 +33,11 @@ class SessionsController < ApplicationController
   end
 
   post '/signup' do
-    binding.pry
     @user = User.new(params[:user])
     if @user.already_a_user? || !(@user.save)
+      @user.errors.full_messages.each do |error_string|
+        flash[:error] = error_string
+      end
       erb :signup
     else
       session[:user_id] = @user.id
