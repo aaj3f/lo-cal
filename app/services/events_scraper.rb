0f5ab75467782a3c9ee0e1348840cc8fed016c54
Andrew Johnson <ajohnson.uva@gmail.com>
2019-01-31 11:28:23 -0500

    added stevens_scraper to EventsScraper

--- app/services/events_scraper.rb
+++ app/services/events_scraper.rb
@@ -3,7 +3,7 @@ class EventsScraper
   def call
     creation_hash = {}
     ramkat_scraper(creation_hash, "https://www.theramkat.com/events")
-    
+    stevens_scraper(creation_hash, "https://www.uncsa.edu/performances/index.aspx")
     create_events(creation_hash)
   end
 
@@ -20,9 +20,9 @@ class EventsScraper
 
     raw_hash = raw_array.each.with_index.with_object({}) do |(item, index), hash|
       if index % 2 == 0
-        hash[item] = {}
+        hash["#{item}/1"] = {}
       else
-        inner_hash = hash[raw_array[(index - 1)]]
+        inner_hash = hash["#{raw_array[(index - 1)]}/1"]
         inner_hash[:name] = item["name"]
         inner_hash[:date_and_time] = item["startDate"]
         inner_hash[:location] = item["location"]["name"]
@@ -34,6 +34,35 @@ class EventsScraper
 
     creation_hash.merge!(raw_hash)
 
+  end
+
+  def stevens_scraper(creation_hash, events_url)
+    base_url = events_url.delete_suffix("/performances/index.aspx")
+
+    doc = Nokogiri::HTML(open(events_url))
+
+    url_array = doc.css("div.grid div a").each.with_object([]) {|node, array| array << node["href"]}
+
+    raw_hash = url_array.each.with_object({}) do |url_suffix, hash|
+      url = base_url + url_suffix
+      inner_doc = Nokogiri::HTML(open(url))
+
+      inner_doc.css('ul.performance__schedule li').each.with_index(1) do |node, index|
+        hash["#{url}/#{index}"] = {}
+        hash["#{url}/#{index}"][:name] = inner_doc.css('div.wrap h1').text
+        hash["#{url}/#{index}"][:date_and_time] = node.text.strip.gsub(/\s+/, " ")
+        hash["#{url}/#{index}"][:location] = inner_doc.css('span.event__location__icon + strong').text
+        hash["#{url}/#{index}"][:organizer] = "UNCSA"
+        if inner_doc.css('p.p--intro span:first-child').first.values.first
+          hash["#{url}/#{index}"][:description] = JSON.parse(inner_doc.css('p.p--intro span:first-child').first.values.first)["2"]
+        else
+          hash["#{url}/#{index}"][:description] = inner_doc.css('p.p--intro').first.text
+        end
+        hash["#{url}/#{index}"][:event_url] = url
+      end
+    end
+
+    creation_hash.merge!(raw_hash)
   end
 
   def create_events(creation_hash)
