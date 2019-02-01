458cbda8613eafbe78169acccc44e3f66e194ac2
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-01 13:06:47 -0500

    fixed error where events with 0 RSVPs showed attendance count

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,7 +3,7 @@ class Event < ActiveRecord::Base
   has_many :users, through: :rsvps
 
   def display_rsvps(current_user)
-    unless self.rsvps.size == 0
+    unless self.rsvps.size < 2
       current_user.events.include?(self) ? user_example = "You" : user_example = self.rsvps.sample.user.first_name
       "<li>#{user_example} & #{self.rsvps.size - 1} #{self.rsvps.size > 2 ? "others" : "other user"} have RSVP'd!</li>"
     end
