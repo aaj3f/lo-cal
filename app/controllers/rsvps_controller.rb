16401380bc41a6d775b1062768410ab2b1c0890c
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-01 09:55:32 -0500

    refactored so that control flow logic was contained in model instance methods instead of erb

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -16,11 +16,21 @@ class RsvpsController < ApplicationController
   patch '/rsvps/:id' do
     event = Event.find_by_id(params[:id])
     new_response = params[:rsvp]
-    user = User.find_by_id(session[:user_id])
-    if event && user && new_response && user.events.include?(event)
-      Rsvp.find_by(user_id: user.id, event_id: event.id).update(status: new_response)
+    if event && logged_in? && new_response && current_user.events.include?(event)
+      Rsvp.find_by(user_id: current_user.id, event_id: event.id).update(status: new_response)
       flash[:notice] = 'We\'ve updated your RSVP!'
       redirect :"/events/#{event.id}"
+    else
+      redirect :"/events"
+    end
+  end
+
+  delete '/rsvps/:id' do
+    event = Event.find_by_id(params[:id])
+    if event && logged_in? && current_user.events.include?(event)
+      Rsvp.find_by(user_id: current_user.id, event_id: event.id).destroy
+      flash[:error] = "We've deleted your RSVP for #{event.name}"
+      redirect :"/events"
     else
       redirect :"/events"
     end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,20 @@
 class Event < ActiveRecord::Base
   has_many :rsvps
   has_many :users, through: :rsvps
+
+  def display_rsvps(current_user)
+    unless self.rsvps.size == 0
+      current_user.events.include?(self) ? user_example = "You" : user_example = self.rsvps.sample.user.first_name
+      "<li>#{user_example} & #{self.rsvps.size - 1} #{self.rsvps.size > 2 ? "others" : "other user"} have RSVP'd!</li>"
+    end
+  end
+
+  def display_date_and_time
+    if self.date_and_time.strftime("%H:%M:%S") == "00:00:00"
+      "#{self.date_and_time.strftime("%A, %B %e")}"
+    else
+      "#{self.date_and_time.strftime("%A, %B %e -- %I:%M%p")}"
+    end
+  end
+
 end
