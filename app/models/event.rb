8bb3eab0f06133f9f8b188bf37d789d0c8daf42b
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-01 17:03:31 -0500

    edited attendance badges on events #index erb to more dynamically represent rsvps

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,9 +3,24 @@ class Event < ActiveRecord::Base
   has_many :users, through: :rsvps
 
   def display_rsvps(current_user)
-    unless self.rsvps.size < 2
-      current_user.events.include?(self) ? user_example = "You" : user_example = self.rsvps.sample.user.first_name
-      "<li>#{user_example} & #{self.rsvps.size - 1} #{self.rsvps.size > 2 ? "others" : "other user"} have RSVP'd!</li>"
+    if current_user.events.include?(self) && self.check_rsvp_status_by_user(current_user) == "decline"
+      "<li><span class=\"badge badge-danger\">You've decided not to attend.</span></li>"
+    elsif self.rsvps.size > 0
+      if current_user.events.include?(self)
+        user_example, badge_class = "You", "badge-success"
+      else
+        user_example, badge_class = self.rsvps.sample.user.first_name, "badge-info"
+      end
+      case self.rsvps.size
+      when 0
+        nil
+      when 1
+        "<li><span class=\"badge #{badge_class}\">#{user_example} have RSVP'd!</span></li>" if user_example == "You"
+      when 2
+        "<li><span class=\"badge #{badge_class}\">#{user_example} & 1 other user have RSVP'd!</span></li>"
+      else
+        "<li><span class=\"badge #{badge_class}\">#{user_example} & #{self.rsvps.size - 1} others have RSVP'd!</span></li>"
+      end
     end
   end
 
@@ -29,6 +44,10 @@ class Event < ActiveRecord::Base
 
   def find_rsvp_by_user(current_user)
     Rsvp.find_by(user_id: current_user.id, event_id: self.id)
+  end
+
+  def check_rsvp_status_by_user(current_user)
+    self.find_rsvp_by_user(current_user).status
   end
 
 end
