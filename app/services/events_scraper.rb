48934c8063849da9e14eeb3df7c39aa9a57d7999
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-01 16:10:10 -0500

    added scraping for Aperture Cinema's Special Events

--- app/services/events_scraper.rb
+++ app/services/events_scraper.rb
@@ -4,6 +4,7 @@ class EventsScraper
     creation_hash = {}
     ramkat_scraper(creation_hash, "https://www.theramkat.com/events")
     stevens_scraper(creation_hash, "https://www.uncsa.edu/performances/index.aspx")
+    aperture_scraper(creation_hash, "https://www.aperturecinema.com/")
     create_events(creation_hash)
   end
 
@@ -65,6 +66,46 @@ class EventsScraper
     creation_hash.merge!(raw_hash)
   end
 
+  def aperture_scraper(creation_hash, events_url)
+    doc = Nokogiri::HTML(open(events_url))
+    events_array = doc.css("div.projects_holder article.mix").select {|node| node.css("span.project_category").text == "Special Events"}
+    raw_hash = events_array.each.with_object({}) do |node, hash|
+      url = node.css("a")[0][:href]
+      inner_doc = Nokogiri::HTML(open(url))
+
+      if inner_doc.css("div.info.portfolio_content p b").text.empty?
+        organizer = "Aperture Cinema"
+      else
+        organizer = inner_doc.css("div.info.portfolio_content p b").text
+      end
+
+      if inner_doc.text.match(/\w+\s\d+\s@\s\d[0-9apm:]+/)
+        date_and_time = DateTime.parse(inner_doc.text.match(/\w+\s\d+\s@\s\d[0-9apm:]+/)[0])
+      else
+        date_and_time = DateTime.parse(inner_doc.css("div.title_subtitle_holder_inner span").text.match(/[^–]+$/)[0].strip)
+      end
+
+
+      hash["#{url}"] = {}
+      hash["#{url}"][:name] = inner_doc.css("div.title_subtitle_holder_inner span").text.match(/^[^–]+/)[0].strip
+      hash["#{url}"][:organizer] = organizer
+      hash["#{url}"][:date_and_time] = date_and_time
+      hash["#{url}"][:location] = "Aperture Cinema"
+      hash["#{url}"][:event_url] = url
+      hash["#{url}"][:description] = inner_doc.css("div.info.portfolio_content p").select {|node| node.text.size > 150 }.each.with_object("") {|node, string| string << "#{node.text}\n"}
+      #name = inner_doc.css("div.title_subtitle_holder_inner span").text.match(/^[^–]+/)[0].strip
+      #location = "Aperture Cinema"
+      #organizer = inner_doc.css("div.info.portfolio_content p b").text
+      #date_and_time = DateTime.parse(inner_doc.css("div.info.portfolio_content p strong").text)
+      #description = inner_doc.css("div.info.portfolio_content p").select {|node| node.text.size > 150 }.each.with_object("") {|node, string| string << "#{node.text}\n"}
+
+      # binding.pry
+    end
+
+    creation_hash.merge!(raw_hash)
+
+  end
+
   def create_events(creation_hash)
     creation_hash.each do |url_key, event_data|
       Event.create(event_data) unless Event.find_by(event_url: event_data[:event_url], date_and_time: event_data[:date_and_time]) || (event_data[:date_and_time] < Date.today)
@@ -72,3 +113,8 @@ class EventsScraper
   end
 
 end
+#
+# elsif inner_doc.css("div.info.portfolio_content p strong").size == 1
+#   date_and_time = DateTime.parse(inner_doc.css("div.info.portfolio_content p strong").text)
+# elsif inner_doc.css("div.info.portfolio_content p strong").size > 1
+#   date_and_time = DateTime.parse(inner_doc.css("div.info.portfolio_content p strong").first.text)
