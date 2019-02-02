e50472ca6a56577a4fa3a9b10eecb50d08cb0aad
Andrew Johnson <ajohnson.uva@gmail.com>
2019-02-02 14:01:31 -0500

    refactored and added flash errors for invalid http requests

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -3,12 +3,12 @@ class RsvpsController < ApplicationController
   post '/rsvps' do
     event = Event.find_by_id(params[:event])
     response = params[:rsvp]
-    user = User.find_by_id(session[:user_id])
-    if event && user && response && !(user.events.include?(event))
-      rsvp = Rsvp.create(user_id: user.id, event_id: event.id, status: response)
-      flash[:notice] = 'We\'ve RSVP\'d you for this event!'
+    if event && logged_in? && response && !(@user.events.include?(event))
+      Rsvp.create(user_id: @user.id, event_id: event.id, status: response)
+      flash[:notice] = "We\'ve RSVP\'d you for #{event.name}!"
       redirect :"/events/#{event.id}"
     else
+      flash[:error] = "Uh-oh! Something went wrong with that RSVP..."
       redirect :"/events"
     end
   end
@@ -16,22 +16,24 @@ class RsvpsController < ApplicationController
   patch '/rsvps/:id' do
     event = Event.find_by_id(params[:id])
     new_response = params[:rsvp]
-    if event && logged_in? && new_response && current_user.events.include?(event)
-      Rsvp.find_by(user_id: current_user.id, event_id: event.id).update(status: new_response)
-      flash[:notice] = 'We\'ve updated your RSVP!'
+    if event && logged_in? && new_response && @user.events.include?(event)
+      Rsvp.find_by(user_id: @user.id, event_id: event.id).update(status: new_response)
+      flash[:notice] = "We\'ve updated your RSVP for #{event.name}!"
       redirect :"/events/#{event.id}"
     else
+      flash[:error] = "Uh-oh! Something went wrong with that RSVP..."
       redirect :"/events"
     end
   end
 
   delete '/rsvps/:id' do
     event = Event.find_by_id(params[:id])
-    if event && logged_in? && current_user.events.include?(event)
-      Rsvp.find_by(user_id: current_user.id, event_id: event.id).destroy
+    if event && logged_in? && @user.events.include?(event)
+      Rsvp.find_by(user_id: @user.id, event_id: event.id).destroy
       flash[:error] = "We've deleted your RSVP for #{event.name}"
       redirect :"/events"
     else
+      flash[:error] = "Uh-oh! Something went wrong with that RSVP..."
       redirect :"/events"
     end
   end
